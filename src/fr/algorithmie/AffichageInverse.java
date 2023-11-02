58d447b5e5ed2215d4ce9a71f2a7d6f0d738882a
Morie <dorian.cormerais@gmail.com>
2023-11-02 17:31:27 +0100

    first commit

---
+++ src/fr/algorithmie/AffichageInverse.java
@@ -0,0 +1,28 @@
+package fr.algorithmie;
+
+public class AffichageInverse {
+    public static void main(String[] args) {
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+        // Afficher l’ensemble des éléments du tableau grâce à une boucle
+        System.out.println("Affichage tableau array");
+        for (int i=0; i<array.length; i++) {
+            System.out.println(array[i]);
+        }
+        // Afficher l’ensemble des éléments dans l’ordre inverse du tableau
+        System.out.println("Affichage ordre inverse tableau array");
+        for (int i=array.length-1;i>=0; i--) {
+            System.out.println(array[i]);
+        }
+        // Créer un tableau arrayCopy et copier tous les éléments de array dans arrayCopy
+        System.out.println("Affichage tableau arrayCopy");
+        int[] arrayCopy = new int[array.length];
+        for (int i=array.length-1;i>=0; i--) {
+            arrayCopy[i] = array[array.length-1-i];
+        }
+        // Afficher le tableau arrayCopy
+        for (int i=0; i<arrayCopy.length; i++) {
+            System.out.println(arrayCopy[i]);
+        }
+    }
+}
